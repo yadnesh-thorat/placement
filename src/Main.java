baaa5641d482ae4ed8996536110157db7cc2ddf5
yadnesh-thorat <yadneshwarthorat@gmail.com>
2025-07-31 21:06:12 +0530

    new

--- src/Main.java
+++ src/Main.java
@@ -454,6 +454,26 @@ import java.util.Scanner;
 
 
 
+class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Number you Want to Reverse it ");
+        int num=sc.nextInt();
+        int rev=0;
+        while(num>0){
+            rev=rev*10+num%10;
+            num=num/10;
+
+//            System.out.println(rev);
+
+        }
+        System.out.println(rev);
+
+
+
+    }
+}
+
 
 
 
